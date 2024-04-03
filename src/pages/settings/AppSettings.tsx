7ecc4a99df9b3bee1f208e95f8ea5f3db8040e29
100mountains <info@brainbunker.org>
2024-04-03 23:15:44 +0000

    update app settings with placeholder buttons

--- src/pages/settings/AppSettings.tsx
+++ src/pages/settings/AppSettings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Button, TextField, styled, Grid, IconButton } from '@mui/material';
+import { Box, Typography, Checkbox, Switch, FormControlLabel, FormGroup, Button, TextField, styled, Grid, IconButton } from '@mui/material';
 import SecurityIcon from '@mui/icons-material/Security'; // You can choose an appropriate icon
 import EmailIcon from '@mui/icons-material/Email';
 import KeyIcon from '@mui/icons-material/VpnKey';
@@ -14,82 +14,62 @@ const SecuritySection = styled(Box)(({ theme }) => ({
   boxShadow: theme.shadows[2], // Add shadow for depth
 }));
 
+
+
+
 function AppSettings() {
+  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Typography variant="h6" gutterBottom>
-        Application Settings
-      </Typography>
-      
-      <Grid container spacing={3}>
-        {/* 2FA Section */}
-        <Grid item xs={12} md={6}>
-          <SecuritySection>
-            <Box display="flex" alignItems="center" marginBottom={2}>
-              <SecurityIcon color="warning" sx={{ marginRight: 2 }} />
-              <Typography variant="h6">2 Factor Authentication</Typography>
-            </Box>
-            <Typography variant="body1" paragraph>
-              Protect your account with an extra layer of security. Once configured you'll be required to enter both your password and an authentication code from your mobile phone in order to sign in.
-            </Typography>
-            <Button variant="contained" color="primary" startIcon={<KeyIcon />}>
-              Enable 2FA
-            </Button>
-          </SecuritySection>
-        </Grid>
+    <Box sx={{ margin: 2, border: '1px solid rgba(0, 0, 0, 0.1)', borderRadius: '10px', padding: 2 }}>
+    <Typography variant="h6" gutterBottom>
+      Application Settings
+    </Typography>
+    
+    <Grid container spacing={3}>
+      {/* Beginner to Normal Mode Section */}
+      <Grid item xs={12} md={6}>
+        <Box sx={{ border: '1px solid rgba(0, 0, 0, 0.1)', borderRadius: '10px', padding: 2, marginBottom: 2 }}>
+          <Typography variant="h6">Mode</Typography>
+          <FormControlLabel
+            control={<Switch name="checkedA" />}
+            label="Beginner Mode"
+          />
+        </Box>
 
-        {/* Change Email and Password Section */}
-        <Grid item xs={12} md={6}>
-          <SecuritySection>
-            {/* Change Email Section */}
-            <Box display="flex" alignItems="center" marginBottom={2}>
-              <EmailIcon sx={{ marginRight: 2 }} />
-              <Typography variant="h6">Change Email Address</Typography>
-            </Box>
-            <TextField
-              fullWidth
-              label="Enter your new email address"
-              variant="outlined"
-              margin="normal"
-            />
-            <Button variant="contained" sx={{ marginTop: 1 }}>
-              Change Email
-            </Button>
-          </SecuritySection>
-
-          {/* Change Password Section */}
-          <SecuritySection>
-            <Box display="flex" alignItems="center" marginBottom={2}>
-              <KeyIcon sx={{ marginRight: 2 }} />
-              <Typography variant="h6">Change Password</Typography>
-            </Box>
-            <TextField
-              fullWidth
-              label="Current Password"
-              type="password"
-              variant="outlined"
-              margin="normal"
-            />
-            <Box display="flex" alignItems="center" marginY={1}>
-              <TextField
-                fullWidth
-                label="New Password"
-                type="password"
-                variant="outlined"
-                margin="normal"
-                sx={{ marginRight: 2 }}
-              />
-              <IconButton>
-                <VisibilityIcon />
-              </IconButton>
-            </Box>
-            <Button variant="contained" sx={{ marginTop: 1 }}>
-              Change Password
-            </Button>
-          </SecuritySection>
-        </Grid>
+        {/* Additional Settings Section */}
+        <Box sx={{ border: '1px solid rgba(0, 0, 0, 0.1)', borderRadius: '10px', padding: 2 }}>
+          <Typography variant="h6">Additional Settings</Typography>
+          <FormControlLabel control={<Checkbox />} label="Enable Feature X" />
+          <FormControlLabel control={<Checkbox />} label="Enable Feature Y" />
+          <TextField
+            fullWidth
+            label="Setting Parameter"
+            variant="outlined"
+            margin="normal"
+          />
+          <Button variant="contained">Apply Changes</Button>
+        </Box>
       </Grid>
-    </Box>
+
+      {/* New Random Settings Section */}
+      <Grid item xs={12} md={6}>
+        <Box sx={{ border: '1px solid rgba(0, 0, 0, 0.1)', borderRadius: '10px', padding: 2 }}>
+          <Typography variant="h6">Random Settings</Typography>
+          <FormControlLabel control={<Checkbox />} label="Enable Random Feature 1" />
+          <FormControlLabel control={<Checkbox />} label="Enable Random Feature 2" />
+          <TextField
+            fullWidth
+            label="Random Setting Input"
+            variant="outlined"
+            margin="normal"
+          />
+          <Button variant="contained">Update Settings</Button>
+        </Box>
+      </Grid>
+    </Grid>
+  </Box>
+  
+  
   );
 }
 
