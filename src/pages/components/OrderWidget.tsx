8904a2f99a5499180843665e20d6041f232346bf
100mountains <info@brainbunker.org>
2024-04-04 19:23:30 +0000

    tvwidget dark mode and orderwidget stuff

--- src/pages/components/OrderWidget.tsx
+++ src/pages/components/OrderWidget.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Box, Typography, Button, Tab, Tabs, TextField, Slider, FormGroup, FormControlLabel, Switch, Collapse } from '@mui/material';
+import { Box, Typography, Button, Tab, Tabs, TextField, Slider, FormGroup, FormControlLabel, Switch, Collapse, IconButton,Tooltip } from '@mui/material';
+
 
 function OrderWidget() {
   const [mainTabValue, setMainTabValue] = useState('buy');
@@ -13,7 +14,7 @@ function OrderWidget() {
   const [expirePosition, setExpirePosition] = useState('');
   const [timeBasedClose, setTimeBasedClose] = useState('');
   const [takeProfitValue, setTakeProfitValue] = useState(''); // State to hold the take profit value
-
+  const [percentage, setPercentage] = useState('0%');
 
   const handleMainTabChange = (event, newValue) => {
     setMainTabValue(newValue);
@@ -23,53 +24,104 @@ function OrderWidget() {
     setOrderType(newValue);
   };
 
+  const percentages = ['5%', '10%', '15%', '25%', '50%', '100%'];
+
   return (
     <div className='orderWidget'>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={mainTabValue} onChange={handleMainTabChange} aria-label="Buy/Sell tabs" variant="fullWidth">
-          <Tab 
-            label={<Typography style={{ width: '100%', backgroundColor: mainTabValue === 'buy' ? 'rgba(0, 128, 0, 0.3)' : undefined }}>Buy</Typography>} 
-            value="buy" 
-          />
-          <Tab 
-            label={<Typography style={{ width: '100%', backgroundColor: mainTabValue === 'sell' ? 'rgba(255, 0, 0, 0.3)' : undefined }}>Sell</Typography>} 
-            value="sell" 
-          />
-        </Tabs>
-      </Box>
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Tabs value={mainTabValue} onChange={handleMainTabChange} aria-label="Buy/Sell tabs" variant="fullWidth">
+        <Tab 
+          label={<Typography style={{ width: '100%', backgroundColor: mainTabValue === 'buy' ? 'rgba(0, 128, 0, 0.3)' : undefined }}>Buy</Typography>} 
+          value="buy" 
+        />
+        <Tab 
+          label={<Typography style={{ width: '100%', backgroundColor: mainTabValue === 'sell' ? 'rgba(255, 0, 0, 0.3)' : undefined }}>Sell</Typography>} 
+          value="sell" 
+        />
+      </Tabs>
+    </Box>
 
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-      <Tabs value={orderType} onChange={handleOrderTypeChange} aria-label="Order type tabs" variant="fullWidth">
-          <Tab 
-            label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>Market</Typography>} 
-            value="market" 
-            sx={{ minWidth: 50 }} // Reducing minimum width to reduce side padding
-          />
-          <Tab 
-            label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>Limit</Typography>} 
-            value="limit" 
-            sx={{ minWidth: 50 }}
-          />
-          <Tab 
-            label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>Ladder</Typography>} 
-            value="ladder" 
-            sx={{ minWidth: 50 }}
-          />
-          <Tab 
-            label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>OCO</Typography>} 
-            value="OCO" 
-            sx={{ minWidth: 50 }}
-          />
-        </Tabs>
-      </Box>
+    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+    <Tabs value={orderType} onChange={handleOrderTypeChange} aria-label="Order type tabs" variant="fullWidth">
+        <Tab 
+          label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>Market</Typography>} 
+          value="market" 
+          sx={{ minWidth: 50 }} // Reducing minimum width to reduce side padding
+        />
+        <Tab 
+          label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>Limit</Typography>} 
+          value="limit" 
+          sx={{ minWidth: 50 }}
+        />
+        <Tab 
+          label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>Ladder</Typography>} 
+          value="ladder" 
+          sx={{ minWidth: 50 }}
+        />
+        <Tab 
+          label={<Typography style={{ fontSize: '0.75rem', padding: '6px 12px' }}>OCO</Typography>} 
+          value="OCO" 
+          sx={{ minWidth: 50 }}
+        />
+      </Tabs>
 
-      <TextField fullWidth value={price} onChange={(e) => setPrice(e.target.value)} label="Price" variant="outlined" margin="normal" />
-      <TextField fullWidth value={spendAmount} onChange={(e) => setSpendAmount(e.target.value)} label="Spend" variant="outlined" margin="normal" />
+    </Box>
+
+    <div className='order-widget-content'>
+      <TextField 
+        className="custom-text-field"
+        fullWidth 
+        value={price} 
+        onChange={(e) => setPrice(e.target.value)} 
+        label="Price" 
+        variant="outlined"
+      />
+
+
+
+<div className="order-container">
+      {/* Percentage Box */}
+      <div className="percentage-box">{percentage}</div>
+
+      {/* Buttons */}
+      <button className="order-button">Bid</button>
+      <button className="order-button">Ask</button>
+      <button className="order-button">Last</button>
+    </div>
+
+
+      <TextField 
+        className="custom-text-field"
+        fullWidth 
+        value={spendAmount} 
+        onChange={(e) => setSpendAmount(e.target.value)} 
+        label="Spend" 
+        variant="outlined"
+      />
+ <div className="order-container">
+      {/* Percentage Box */}
+      <div className="percentage-box">{percentage}</div>
+
+      {/* Percentage Buttons */}
+      {percentages.map((value) => (
+        <button key={value} className="order-button" onClick={() => setPercentage(value)}>
+          {value}
+        </button>
+      ))}
+    </div>
+
       <Box>
         <Typography variant="body2">Spend Percentage:</Typography>
         <Slider value={spendPercentage} onChange={(e, newValue) => setSpendPercentage(newValue as number)} step={5} marks min={0} max={100} valueLabelDisplay="auto" />
       </Box>
-      <TextField fullWidth value={buyAmount} onChange={(e) => setBuyAmount(e.target.value)} label="Buy" variant="outlined" margin="normal" />
+      <TextField 
+        className="custom-text-field"
+        fullWidth 
+        value={buyAmount} 
+        onChange={(e) => setBuyAmount(e.target.value)} 
+        label="Buy" 
+        variant="outlined"
+      />
       <FormGroup>
         <FormControlLabel
           control={<Switch checked={takeProfit} onChange={(e) => setTakeProfit(e.target.checked)} size="small" />}
@@ -77,12 +129,12 @@ function OrderWidget() {
         />
         <Collapse in={takeProfit}>
           <TextField
+            className="custom-text-field"
             fullWidth
             value={takeProfitValue}
             onChange={(e) => setTakeProfitValue(e.target.value)}
             label="Take Profit Value"
             variant="outlined"
-            margin="normal"
           />
           {/* Additional fields or components can go here */}
         </Collapse>
@@ -92,14 +144,30 @@ function OrderWidget() {
         />
       </FormGroup>
 
-      <TextField fullWidth value={expirePosition} onChange={(e) => setExpirePosition(e.target.value)} label="Expire position at" variant="outlined" margin="normal" />
-      <TextField fullWidth value={timeBasedClose} onChange={(e) => setTimeBasedClose(e.target.value)} label="Time-Based auto close" variant="outlined" margin="normal" />
+      <TextField 
+        className="custom-text-field"
+        fullWidth 
+        value={expirePosition} 
+        onChange={(e) => setExpirePosition(e.target.value)} 
+        label="Expire position at" 
+        variant="outlined"
+      />
+      <TextField 
+        className="custom-text-field"
+        fullWidth 
+        value={timeBasedClose} 
+        onChange={(e) => setTimeBasedClose(e.target.value)} 
+        label="Time-Based auto close" 
+        variant="outlined"
+      />
 
       <Button variant="contained" color={mainTabValue === 'buy' ? "success" : "error"} fullWidth>
         {mainTabValue === 'buy' ? 'Buy' : 'Sell'}
       </Button>
     </div>
-  );
+  </div>
+);
+
 }
 
 export default OrderWidget;
--- src/pages/components/TVWidget.tsx
+++ src/pages/components/TVWidget.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useTradingPair } from './TradingPairContext';
+import { DarkModeContext } from './DarkModeContext'; // Import the DarkModeContext
 
-
-export default  class Tabsshow extends React.PureComponent {
+export default class Tabsshow extends React.PureComponent {
     constructor(props) {
         super(props);
         this._ref = React.createRef();
@@ -13,12 +13,15 @@ export default  class Tabsshow extends React.PureComponent {
         script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js"; // Correct script URL
         script.async = true;
 
+        // Retrieve dark mode context value
+        const darkModeContext = this.context;
+
         const widgetConfig = {
            "autosize": true,
            "symbol": "BITSTAMP:BTCUSD",
            "interval": "D",
            "timezone": "Etc/UTC",
-           "theme": "light",
+           "theme": darkModeContext.darkMode ? "dark" : "light", // Dynamically set theme based on dark mode context
            "style": "1",
            "locale": "en",
            "enable_publishing": false,
@@ -27,26 +30,25 @@ export default  class Tabsshow extends React.PureComponent {
            "allow_symbol_change": true,
            "calendar": false,
            "studies": [
-            "STD;Average_True_Range",
-            "STD;Bollinger_Bands",
-            "STD;Connors_RSI",
-            "STD;DEMA",
-            "STD;Ichimoku%1Cloud",
-            "STD;MA%1Cross",
-            "STD;Momentum",
-            "STD;EMA",
-            "STD;WMA",
-            "STD;PSAR",
-            "PriceVolumeTrend@tv-basicstudies",
-            "STD;RSI",
-            "STD;Divergence%1Indicator",
-            "STD;Stochastic",
-            "STD;Stochastic_RSI",
-            "Volume@tv-basicstudies",
-            "STD;VWMA",
-
-          ],
-          "support_host": "https://www.tradingview.com"
+                "STD;Average_True_Range",
+                "STD;Bollinger_Bands",
+                "STD;Connors_RSI",
+                "STD;DEMA",
+                "STD;Ichimoku%1Cloud",
+                "STD;MA%1Cross",
+                "STD;Momentum",
+                "STD;EMA",
+                "STD;WMA",
+                "STD;PSAR",
+                "PriceVolumeTrend@tv-basicstudies",
+                "STD;RSI",
+                "STD;Divergence%1Indicator",
+                "STD;Stochastic",
+                "STD;Stochastic_RSI",
+                "Volume@tv-basicstudies",
+                "STD;VWMA",
+            ],
+            "support_host": "https://www.tradingview.com"
         };
 
         script.innerHTML = JSON.stringify(widgetConfig);
@@ -55,13 +57,19 @@ export default  class Tabsshow extends React.PureComponent {
 
     render() {
         return (
-            <div className="tradingview-widget-container" ref={this._ref} style={{ height: '100%', width: '100%' }}>
-                <div className="tradingview-widget-container__widget" style={{ height: 'calc(100% - 32px)', width: '100%' }}>
-                </div>
-                <div className="tradingview-widget-copyright">
-                    {/* ... Your copyright content ... */}
-                </div>
-            </div>
+            <DarkModeContext.Consumer>
+                {darkModeContext => (
+                    <div className="tradingview-widget-container" ref={this._ref}>
+                        <div className="tradingview-widget-container__widget"></div>
+                        <div className="tradingview-widget-copyright">
+                            {/* ... Your copyright content ... */}
+                        </div>
+                    </div>
+                )}
+            </DarkModeContext.Consumer>
         );
     }
 }
+
+// Use context to access dark mode state
+Tabsshow.contextType = DarkModeContext;
