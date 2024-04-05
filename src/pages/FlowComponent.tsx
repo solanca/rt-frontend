728a3eab02bfd0b00566da93c63880688b233306
sparqxi <aldo@code3studio.com>
2024-04-05 01:43:06 -0700

    Flow Page Types

--- src/pages/FlowComponent.tsx
+++ src/pages/FlowComponent.tsx
@@ -1,39 +1,48 @@
-import React, { useContext, useState, useRef, useCallback } from 'react';
-import { ReactFlow, addEdge, useNodesState, useEdgesState, OnConnect, MiniMap, Background, useReactFlow} from '@xyflow/react';
+import { useContext, useState, useCallback } from "react";
+import {
+  ReactFlow,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  OnConnect,
+  MiniMap,
+  Background,
+  useReactFlow,
+  BackgroundVariant,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
-import { DarkModeContext } from './components/DarkModeContext'; // Adjust the path to your DarkModeContext
-import { nodes as initialNodes, edges as initialEdges } from './flow/turbo-elements';
-import './flow/index.css';
-import TurboNode, { TurboNodeData } from './flow/TurboNode';
-import TurboEdge from './flow/TurboEdge';
-import FunctionIcon from './flow/FunctionIcon';
-import { FiCloud, FiFile } from 'react-icons/fi';
-import SignalsTopBar from './topbars/SignalsTopBar';
-import {AppBar }from '@mui/material';
-
+import { DarkModeContext } from "./components/DarkModeContext"; // Adjust the path to your DarkModeContext
+import {
+  nodes as initialNodes,
+  edges as initialEdges,
+} from "./flow/turbo-elements";
+import "./flow/index.css";
+import TurboNode from "./flow/TurboNode";
+import TurboEdge from "./flow/TurboEdge";
+import SignalsTopBar from "./topbars/SignalsTopBar";
 
 const FlowComponent = () => {
   const { darkMode } = useContext(DarkModeContext);
   const reactFlowInstance = useReactFlow();
 
-const nodeTypes = {
-  turbo: TurboNode,
-};
+  const nodeTypes:any = {
+    turbo: TurboNode,
+  };
 
-const edgeTypes = {
-  turbo: TurboEdge,
-};
+  const edgeTypes = {
+    turbo: TurboEdge,
+  };
 
-const defaultEdgeOptions = {
-  type: 'turbo',
-  // markerEnd: 'edge-circle',
-};
+  const defaultEdgeOptions = {
+    type: "turbo",
+    // markerEnd: 'edge-circle',
+  };
 
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [background, setBackground] = useState('lines');  // Default background
+  const [background, setBackground] = useState<BackgroundVariant|undefined>(BackgroundVariant.Lines); // Default background
 
-  const handleBackgroundChange = (newBackground) => {
+  const handleBackgroundChange = (newBackground:BackgroundVariant) => {
     setBackground(newBackground);
   };
 
@@ -42,85 +51,86 @@ const defaultEdgeOptions = {
     [setEdges]
   );
 
-const onDrop = useCallback((event) => {
-  event.preventDefault();
+  const onDrop = useCallback(
+    (event:any) => {
+      event.preventDefault();
 
-  if (!reactFlowInstance) {
-    return;
-  }
+      if (!reactFlowInstance) {
+        return;
+      }
 
-  const type = event.dataTransfer.getData('application/reactflow');
-  if (!type) {
-    return;
-  }
+      const type = event.dataTransfer.getData("application/reactflow");
+      if (!type) {
+        return;
+      }
 
-  const position = reactFlowInstance.screenToFlowPosition({
-    x: event.clientX,
-    y: event.clientY,
-  });
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
 
-const newNode = {
-  id: `${type}_${new Date().getTime()}`,
-  type: 'turbo',  // Use your custom node type here
-  position,
-  data: { label: `${type} node` },
-};
+      const newNode = {
+        id: `${type}_${new Date().getTime()}`,
+        type: "turbo", // Use your custom node type here
+        position,
+        data: { label: `${type} node` },
+      };
 
-  setNodes((nds) => nds.concat(newNode));
-}, [reactFlowInstance, setNodes]);
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance, setNodes]
+  );
 
-  const colorMode = darkMode ? 'dark' : 'light';
+  const colorMode = darkMode ? "dark" : "light";
 
+  return (
+    <>
+      <SignalsTopBar onBackgroundChange={handleBackgroundChange} />
 
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onDrop={onDrop}
+        onDragOver={(event) => event.preventDefault()}
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        defaultEdgeOptions={defaultEdgeOptions}
+        colorMode={colorMode}
+        fitView
+      >
+        <svg>
+          <defs>
+            <linearGradient id="edge-gradient">
+              <stop offset="0%" stopColor="#ae53ba" />
+              <stop offset="100%" stopColor="#2a8af6" />
+            </linearGradient>
 
-
-return (
-
-<>
-
-<SignalsTopBar onBackgroundChange={handleBackgroundChange} />
- 
-
-    <ReactFlow
-      nodes={nodes}
-      edges={edges}
-      onNodesChange={onNodesChange}
-      onEdgesChange={onEdgesChange}
-      onConnect={onConnect}
-      onDrop={onDrop}
-      onDragOver={(event) => event.preventDefault()}      
-      nodeTypes={nodeTypes}
-      edgeTypes={edgeTypes}
-      defaultEdgeOptions={defaultEdgeOptions}
-      colorMode={colorMode}
-      fitView
-      
-    >
-     
-      <svg>
-        <defs>
-          <linearGradient id="edge-gradient">
-            <stop offset="0%" stopColor="#ae53ba" />
-            <stop offset="100%" stopColor="#2a8af6" />
-          </linearGradient>
-
-          <marker
-            id="edge-circle"
-            viewBox="-5 -5 10 10"
-            refX="0"
-            refY="0"
-            markerUnits="strokeWidth"
-            markerWidth="10"
-            markerHeight="10"
-            orient="auto"
-          >
-            <circle stroke="#2a8af6" strokeOpacity="0.75" r="2" cx="0" cy="0" />
-          </marker>
-        </defs>
-      </svg>
-      <MiniMap />
-      <Background variant={background} />
-    </ReactFlow>
+            <marker
+              id="edge-circle"
+              viewBox="-5 -5 10 10"
+              refX="0"
+              refY="0"
+              markerUnits="strokeWidth"
+              markerWidth="10"
+              markerHeight="10"
+              orient="auto"
+            >
+              <circle
+                stroke="#2a8af6"
+                strokeOpacity="0.75"
+                r="2"
+                cx="0"
+                cy="0"
+              />
+            </marker>
+          </defs>
+        </svg>
+        <MiniMap />
+        <Background variant={background} />
+      </ReactFlow>
     </>
   );
 };
