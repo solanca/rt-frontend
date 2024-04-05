3149908095a2e6224544597cfcae9add7d2ea815
sparqxi <zhongxi1992@gmail.com>
2024-04-05 04:56:00 -0700

    settings page types

--- src/pages/SettingsComponent.tsx
+++ src/pages/SettingsComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, Tabs, Tab, Checkbox, FormGroup, FormControlLabel } from '@mui/material';
+import { Box, Tabs, Tab } from '@mui/material';
 import WebsocketSettings from './settings/WebsocketSettings';
 import UserProfileSettings from './settings/UserProfileSettings';
 import ExchangeAPISettings from './settings/ExchangeAPISettings';
@@ -12,70 +12,70 @@ import SettingsTopBar from './topbars/SettingsTopBar';
 
 function SettingsComponent() {
   const [value, setValue] = useState(0);
-  const [signals, setSignals] = useState([]); // Keep this if it's used elsewhere in your component or future updates
+  // const [signals, setSignals] = useState([]); // Keep this if it's used elsewhere in your component or future updates
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (_event: React.SyntheticEvent<Element, Event>, newValue:any) => {
     setValue(newValue);
   };
 
-  const [exchangeChecked, setExchangeChecked] = useState({
-    kucoin: false,
-    binance: false,
-    kraken: false,
-    coinbase: false,
-  });
+  // const [exchangeChecked, setExchangeChecked] = useState({
+  //   kucoin: false,
+  //   binance: false,
+  //   kraken: false,
+  //   coinbase: false,
+  // });
 
-  const [apiChecked, setApiChecked] = useState({
-    api1: false,
-    api2: false,
-    api3: false,
-    api4: false,
-  });
+  // const [apiChecked, setApiChecked] = useState({
+  //   api1: false,
+  //   api2: false,
+  //   api3: false,
+  //   api4: false,
+  // });
 
-  const handleExchangeChange = (event) => {
-    setExchangeChecked({
-      ...exchangeChecked,
-      [event.target.name]: event.target.checked,
-    });
-  };
+//   const handleExchangeChange = (event) => {
+//     setExchangeChecked({
+//       ...exchangeChecked,
+//       [event.target.name]: event.target.checked,
+//     });
+//   };
 
-  const handleApiChange = (event) => {
-    setApiChecked({
-      ...apiChecked,
-      [event.target.name]: event.target.checked,
-    });
-  };
+//   const handleApiChange = (event) => {
+//     setApiChecked({
+//       ...apiChecked,
+//       [event.target.name]: event.target.checked,
+//     });
+//   };
 
-const handleClearDatabase = async () => {
-  try {
-    const response = await fetch('http://127.0.0.1:8080/api/clear_database', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (!response.ok) throw new Error(`Error clearing database: ${response.status}`);
-    alert('Database cleared successfully');
-  } catch (error) {
-    console.error("Error clearing database:", error);
-    alert(`Error clearing database: ${error.message}`);
-  }
-};
+// const handleClearDatabase = async () => {
+//   try {
+//     const response = await fetch('http://127.0.0.1:8080/api/clear_database', {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//     });
+//     if (!response.ok) throw new Error(`Error clearing database: ${response.status}`);
+//     alert('Database cleared successfully');
+//   } catch (error) {
+//     console.error("Error clearing database:", error);
+//     alert(`Error clearing database: ${error.message}`);
+//   }
+// };
 
-const boxStyle = {
-  display: 'flex',
-  flexDirection: 'row', // Align items horizontally
-  justifyContent: 'flex-start', // Align content to the start of the main axis
-  alignItems: 'flex-start', // Align items to the start of the cross axis
-  height: 'auto', // Height is determined by the content
-  border: '1px solid #333',
-  boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
-  width: '80%', // Responsive width
-  maxWidth: '1200px', // Wider layout
-  marginTop: '10px', // 10px space at the top
-  marginLeft: 'auto', // Center the box horizontally
-  marginRight: 'auto', // Center the box horizontally
-  padding: '20px',
-  overflow: 'hidden', // Prevent overflow
-};
+// const boxStyle = {
+//   display: 'flex',
+//   flexDirection: 'row', // Align items horizontally
+//   justifyContent: 'flex-start', // Align content to the start of the main axis
+//   alignItems: 'flex-start', // Align items to the start of the cross axis
+//   height: 'auto', // Height is determined by the content
+//   border: '1px solid #333',
+//   boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+//   width: '80%', // Responsive width
+//   maxWidth: '1200px', // Wider layout
+//   marginTop: '10px', // 10px space at the top
+//   marginLeft: 'auto', // Center the box horizontally
+//   marginRight: 'auto', // Center the box horizontally
+//   padding: '20px',
+//   overflow: 'hidden', // Prevent overflow
+// };
 
 
 const settingsPanelStyle = {
